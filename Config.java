595a70a0080dcb93b62f04740d22f7e92176c508
loozen <loozen120@gmail.com>
2024-10-19 16:16:51 +0300

    Create Config.java

---
+++ Config.java
@@ -0,0 +1,22 @@
+package ru.loozen.destyautomessages;
+
+public class Config {
+    private String message;
+    private int delay;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+}
